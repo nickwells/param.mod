c7c9f553c664cfa2c0a5dc9ba5b5bff8551140f7
Nick Wells <wells.nicholas@gmail.com>
2019-05-27 17:03:15 +0100

    showParameterSources now also shows group parameter files

--- v3/param/phelp/paramSources.go
+++ v3/param/phelp/paramSources.go
@@ -2,27 +2,16 @@ package phelp
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/nickwells/param.mod/v3/param"
 	"github.com/nickwells/twrap.mod/twrap"
 )
 
-// showParamSources will print a usage message showing the alternative
-// sources that can be used to set parameters: environment variables or
-// configuration files.
-func showParamSources(ps *param.PSet) {
-	cf := ps.ConfigFiles()
-	ep := ps.EnvPrefixes()
-
-	w := ps.StdWriter()
-
-	fmt.Fprintln(w, "\nAdditional Sources")
-	if len(cf) == 0 && len(ep) == 0 {
-		fmt.Fprintln(w, "None")
-		return
-	}
-
+// showConfigFiles prints the config files that can be used to configure the
+// behaviour of the program
+func showConfigFiles(w io.Writer, cf []param.ConfigFileDetails) {
 	if len(cf) != 0 {
 		fmt.Fprintln(w, "  Configuration Files")
 
@@ -30,7 +19,28 @@ func showParamSources(ps *param.PSet) {
 			fmt.Fprintln(w, "    ", f.String())
 		}
 	}
+}
 
+type groupCF struct {
+	groupName string
+	cf        param.ConfigFileDetails
+}
+
+// showGroupConfigFiles prints the config files specific to particular groups
+// of parameters that can be used to configure the behaviour of the program
+func showGroupConfigFiles(w io.Writer, gf []groupCF) {
+	if len(gf) != 0 {
+		fmt.Fprintln(w, "  Group Configuration Files")
+
+		for _, f := range gf {
+			fmt.Fprintln(w, "    "+f.groupName+": ", f.cf.String())
+		}
+	}
+}
+
+// showEnvironmentVariables prints the config files specific to particular groups
+// of parameters that can be used to configure the behaviour of the program
+func showEnvironmentVariables(w io.Writer, ep []string) {
 	if len(ep) != 0 {
 		fmt.Fprintln(w, "  Environment Variables")
 
@@ -39,6 +49,37 @@ func showParamSources(ps *param.PSet) {
 			fmt.Fprint(os.Stderr, "Couldn't build the text wrapper:", err)
 			return
 		}
-		twc.Wrap(altSrcEnvVars(ep), 4)
+		twc.Wrap("The program can also be configured "+altSrcEnvVars(ep), 4)
 	}
+}
+
+// showParamSources will print a usage message showing the alternative
+// sources that can be used to set parameters: environment variables or
+// configuration files.
+func showParamSources(ps *param.PSet) {
+	cf := ps.ConfigFiles()
+	gf := []groupCF{}
+	ep := ps.EnvPrefixes()
+
+	groups := ps.GetGroups()
+	for _, grp := range groups {
+		for _, configFile := range grp.ConfigFiles {
+			gf = append(gf, groupCF{
+				groupName: grp.Name,
+				cf:        configFile,
+			})
+		}
+	}
+
+	w := ps.StdWriter()
+
+	fmt.Fprintln(w, "\nAdditional Sources")
+	if len(cf) == 0 && len(gf) == 0 && len(ep) == 0 {
+		fmt.Fprintln(w, "None")
+		return
+	}
+
+	showConfigFiles(w, cf)
+	showGroupConfigFiles(w, gf)
+	showEnvironmentVariables(w, ep)
 }
