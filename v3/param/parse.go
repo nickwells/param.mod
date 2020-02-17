68f12a357331717ccb02cf85ce0ef776a149cafb
Nick Wells <wells.nicholas@gmail.com>
2020-02-17 18:53:46 +0000

    fix the error message reporting that Parse has already been called

--- v3/param/parse.go
+++ v3/param/parse.go
@@ -54,6 +54,9 @@ func (ps *PSet) Parse(args ...[]string) ErrMap {
 		return errMap
 	}
 
+	ps.parsed = true
+	ps.parseCalledFrom = caller()
+
 	if len(args) == 0 {
 		ps.progName = os.Args[0]
 		ps.progBaseName = filepath.Base(ps.progName)
@@ -80,10 +83,9 @@ func (ps *PSet) Parse(args ...[]string) ErrMap {
 	for _, fcf := range ps.finalChecks {
 		err := fcf()
 		if err != nil {
-			ps.errors[""] = append(ps.errors[""], err)
+			ps.errors["Final Checks"] = append(ps.errors["Final Checks"], err)
 		}
 	}
-	ps.setParsed()
 
 	ps.helper.ProcessArgs(ps)
 	ps.helper.ErrorHandler(ps.ErrWriter(), ps.ProgName(), ps.errors)
@@ -93,10 +95,14 @@ func (ps *PSet) Parse(args ...[]string) ErrMap {
 	return ps.errors
 }
 
-// caller returns a string giving the filename and line number of the calling
-// function. This is intended for providing useful debugging messages
+// caller returns a string giving the filename and line number of the caller
+// of the calling function. This is intended for providing useful debugging
+// messages. Note that we ask for the second stack entry above this: 0 would
+// give the location of the call to runtime.Caller, 1 would give the location
+// of the call to caller() but we want to see where the parent function was
+// called so we pass 2
 func caller() string {
-	if pc, file, line, ok := runtime.Caller(1); ok {
+	if pc, file, line, ok := runtime.Caller(2); ok {
 		f := runtime.FuncForPC(pc)
 		funcName := "unknown"
 		if f != nil {
@@ -105,13 +111,6 @@ func caller() string {
 		return fmt.Sprintf("%s:%d [%s]", file, line, funcName)
 	}
 	return "unknown-file:0 [unknown]"
-}
-
-// setParsed records that the parameters have already been parsed and where
-// it has been called from
-func (ps *PSet) setParsed() {
-	ps.parsed = true
-	ps.parseCalledFrom = caller()
 }
 
 func (ps *PSet) detectMandatoryParamsNotSet() {
