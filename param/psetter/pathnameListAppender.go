236b318e7c8291683707a5ba8a59592aa83ddd29
Nick Wells <wells.nicholas@gmail.com>
2020-07-05 13:51:36 +0100

    Add the PathnameAppender setter
    This will add a pathname parameter to a list of pathnames

---
+++ param/psetter/pathnameListAppender.go
@@ -0,0 +1,110 @@
+package psetter
+
+import (
+	"github.com/nickwells/check.mod/check"
+	"github.com/nickwells/filecheck.mod/filecheck"
+	"github.com/nickwells/fileparse.mod/fileparse"
+)
+
+// PathnameListAppender allows you to specify a parameter that can be used to
+// add to a list (a slice) of pathnames.
+//
+// The user of the program which has a parameter of this type can pass
+// multiple parameters and each will add to the list of values rather than
+// replacing it each time. Note that each value must be passed separately;
+// there is no way to pass multiple values at the same time. Also note that
+// there is no way to reset the value, if this feature is required another
+// parameter could be set up that will do this.
+type PathnameListAppender struct {
+	ValueReqMandatory
+
+	// You must set a Value, the program will panic if not. This is the slice
+	// of strings that the setter is appending to.
+	Value *[]string
+	// Expectation allows you to set some file-specific checks.
+	Expectation filecheck.Provisos
+	// The Checks, if any, are applied to the supplied parameter value and
+	// the new parameter will be added to the list only if they all return a
+	// nil error
+	Checks []check.String
+	// Prepend will change the behaviour so that any new values are added at
+	// the start of the list of pathnames rather than the end
+	Prepend bool
+}
+
+// CountChecks returns the number of check functions this setter has
+func (s PathnameListAppender) CountChecks() int {
+	return len(s.Checks)
+}
+
+// SetWithVal (called when a value follows the parameter) takes the parameter
+// value and runs the checks against it. If any check returns a non-nil error
+// it will return the error. Otherwise it will apply the Editor (if there is
+// one) to the parameter value. If the Editor returns a non-nil error then
+// that is returned and the Value is left unchanged.  Finally, it will append
+// the checked and possibly edited value to the slice of strings.
+func (s PathnameListAppender) SetWithVal(paramName, paramVal string) error {
+	pathname, err := fileparse.FixFileName(paramVal)
+	if err != nil {
+		return err
+	}
+
+	err = s.Expectation.StatusCheck(pathname)
+	if err != nil {
+		return err
+	}
+
+	for _, check := range s.Checks {
+		if check == nil {
+			continue
+		}
+
+		err := check(pathname)
+		if err != nil {
+			return err
+		}
+	}
+
+	if s.Prepend {
+		*s.Value = append([]string{pathname}, *s.Value...)
+		return nil
+	}
+	*s.Value = append(*s.Value, pathname)
+
+	return nil
+}
+
+// AllowedValues returns a description of the allowed values. It includes the
+// separator to be used
+func (s PathnameListAppender) AllowedValues() string {
+	const (
+		intro = "a pathname that will be added to the"
+		outro = " existing list of values"
+	)
+	prepend := ""
+	if s.Prepend {
+		prepend = " start of the"
+	}
+	return intro + prepend + outro + HasChecks(s)
+}
+
+// CurrentValue returns the current setting of the parameter value
+func (s PathnameListAppender) CurrentValue() string {
+	cv := ""
+	sep := ""
+
+	for _, v := range *s.Value {
+		cv += sep + v
+		sep = "\n"
+	}
+
+	return cv
+}
+
+// CheckSetter panics if the setter has not been properly created - if the
+// Value is nil.
+func (s PathnameListAppender) CheckSetter(name string) {
+	if s.Value == nil {
+		panic(NilValueMessage(name, "psetter.PathnameListAppender"))
+	}
+}
---
+++ param/psetter/pathnameListAppenderExamples_test.go
@@ -0,0 +1,63 @@
+package psetter_test
+
+import (
+	"fmt"
+
+	"github.com/nickwells/check.mod/check"
+	"github.com/nickwells/filecheck.mod/filecheck"
+	"github.com/nickwells/param.mod/v5/param/psetter"
+)
+
+// ExamplePathnameListAppender_standard demonstrates the use of a PathnameListAppender
+func ExamplePathnameListAppender_standard() {
+	ps := newPSetForTesting() // you would normally use paramset.NewOrDie()
+
+	ss := []string{"testdata/pathname/nonesuch.go"}
+
+	ps.Add("next",
+		psetter.PathnameListAppender{
+			Value:       &ss,
+			Expectation: filecheck.IsNew(),
+			Checks: []check.String{
+				check.StringHasSuffix(".go"),
+			},
+		},
+		"help text")
+
+	fmt.Println("Before parsing")
+	for i, v := range ss {
+		fmt.Printf("\tss[%d] = %q\n", i, v)
+	}
+	ps.Parse([]string{"-next", "testdata/pathname/nonesuch2.go"})
+	fmt.Println("After  parsing")
+	for i, v := range ss {
+		fmt.Printf("\tss[%d] = %q\n", i, v)
+	}
+	// Output:
+	// Before parsing
+	//	ss[0] = "testdata/pathname/nonesuch.go"
+	// After  parsing
+	//	ss[0] = "testdata/pathname/nonesuch.go"
+	//	ss[1] = "testdata/pathname/nonesuch2.go"
+}
+
+// ExamplePathnameListAppender_withNilValue demonstrates the behaviour of the
+// package when an invalid setter is provided. In this case the Value to be
+// set has not been initialised. Note that in production code you should not
+// recover from the panic, instead you should fix the code that caused it.
+func ExamplePathnameListAppender_withNilValue() {
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println("panic")
+			fmt.Println(p)
+		}
+	}()
+
+	ps := newPSetForTesting() // you would normally use paramset.NewOrDie()
+
+	// we expect this to panic because the list Value has not been initialised
+	ps.Add("my-list", psetter.PathnameListAppender{}, "help text")
+	// Output:
+	// panic
+	// my-list: psetter.PathnameListAppender Check failed: the Value to be set is nil
+}
