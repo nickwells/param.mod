13de38eed725f6af77e45cfcefe0099303536b4f
Nick Wells <wells.nicholas@gmail.com>
2023-09-10 23:43:58 +0100

    refactor(paramset): move the noHelp type to a separate file

--- paramset/std.go
+++ paramset/std.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/nickwells/param.mod/v6/param"
 	"github.com/nickwells/param.mod/v6/phelp"
-	"github.com/nickwells/twrap.mod/twrap"
 )
 
 // addHelperToOpts will take the slice of param set option functions and a
@@ -52,43 +51,4 @@ func NewOrPanic(psof ...param.PSetOptFunc) *param.PSet {
 		panic(fmt.Errorf("The program parameter set can't be made: %w", err))
 	}
 	return ps
-}
-
-// noHelp is a minimal implementation of the param.Helper interface. In
-// particular there are no parameters added
-type noHelp struct{}
-
-func (nh noHelp) ProcessArgs(_ *param.PSet)       {}
-func (nh noHelp) Help(_ *param.PSet, _ ...string) {}
-func (nh noHelp) AddParams(_ *param.PSet)         {}
-func (nh noHelp) ErrorHandler(ps *param.PSet, errs param.ErrMap) {
-	if len(errs) == 0 {
-		return
-	}
-	twc, err := twrap.NewTWConf(twrap.SetWriter(ps.ErrW()))
-	if err != nil {
-		panic(fmt.Sprint("Couldn't build the text wrapper:", err))
-	}
-
-	phelp.ReportErrors(twc, ps.ProgName(), errs)
-
-	os.Exit(1)
-}
-
-var nh noHelp
-
-// NewNoHelp creates a new PSet with the helper set to the noHelp
-// helper which does nothing. In particular it will not add any parameters
-// and so it returns a suitable parameter set for the case where you want to
-// add positional parameters the last of which is terminal. This style of
-// interface is used if you have a positional parameter which will invoke a
-// different command based on the value - see the 'git' or 'go' commands for
-// examples of this CLI interface style. If you are choosing an interface
-// like this you might want to consider having one of the possible parameter
-// values being "help" so that the available options can be listed.
-//
-// If errors are detected then they will be reported and the program will
-// exit.
-func NewNoHelp(psof ...param.PSetOptFunc) (*param.PSet, error) {
-	return param.NewSet(append(psof, param.SetHelper(nh))...)
 }
