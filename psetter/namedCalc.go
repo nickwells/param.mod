5b6473c84ce97758b0af8d5fdd3f1d28ab629ab2
Nick Wells <wells.nicholas@gmail.com>
2023-09-22 22:15:18 +0100

    feature(psetter): added some funcs to generate specific NamedCalcs

--- psetter/namedCalc.go
+++ psetter/namedCalc.go
@@ -1,6 +1,14 @@
 package psetter
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/nickwells/mathutil.mod/v2/mathutil"
+	"golang.org/x/exp/constraints"
+)
 
 // NamedCalc holds a function and associated text. The text can be used in
 // parameter descriptions to describe what the Calc function does. The Calc
@@ -21,4 +29,116 @@ func (tc NamedCalc[T]) Check() error {
 		return errors.New("the Calc must not be nil")
 	}
 	return nil
+}
+
+// ConstCalc returns a NamedCalc of the correct type which will set the value
+// to the supplied value
+func ConstCalc[T any](name string, val T) NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: name,
+		Calc: func(_, _ string) (T, error) {
+			return val, nil
+		},
+	}
+}
+
+// Env2IntCalc returns a NamedCalc of the correct type which will return the
+// value of the given environment variable converted to the appropriate
+// signed integer type
+func Env2IntCalc[T constraints.Signed](name, envName string) NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: name,
+		Calc: func(_, _ string) (T, error) {
+			evVal := os.Getenv(envName)
+			if evVal == "" {
+				return 0, fmt.Errorf(
+					"the %s environment variable is not set",
+					envName)
+			}
+			v, err := strconv.ParseInt(evVal, 0, mathutil.BitsInType(T(0)))
+			if err != nil {
+				return 0, fmt.Errorf(
+					"bad %s environment variable value: %q: %w",
+					envName, evVal, err)
+			}
+			return T(v), nil
+		},
+	}
+}
+
+// Env2UintCalc returns a NamedCalc of the correct type which will return the
+// value of the given environment variable converted to the appropriate
+// unsigned integer type
+func Env2UintCalc[T constraints.Unsigned](name, envName string) NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: name,
+		Calc: func(_, _ string) (T, error) {
+			evVal := os.Getenv(envName)
+			if evVal == "" {
+				return 0, fmt.Errorf(
+					"the %s environment variable is not set",
+					envName)
+			}
+			v, err := strconv.ParseUint(evVal, 0, mathutil.BitsInType(T(0)))
+			if err != nil {
+				return 0, fmt.Errorf(
+					"bad %s environment variable value: %q: %w",
+					envName, evVal, err)
+			}
+			return T(v), nil
+		},
+	}
+}
+
+// Val2IntCalc returns a NamedCalc of the correct type which will set the
+// value to the value of the parameter value converted to the
+// appropriate signed integer type
+func Val2IntCalc[T constraints.Signed]() NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: "a parameter that can be converted to an integer",
+		Calc: func(_, paramVal string) (T, error) {
+			v, err := strconv.ParseInt(paramVal, 0, mathutil.BitsInType(T(0)))
+			if err != nil {
+				return 0, fmt.Errorf("bad parameter: %q: %w", paramVal, err)
+			}
+			return T(v), nil
+		},
+	}
+}
+
+// Val2UintCalc returns a NamedCalc of the correct type which will return the
+// parameter value converted to the appropriate unsigned integer type
+func Val2UintCalc[T constraints.Unsigned]() NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: "a parameter that can be converted to an unsigned integer",
+		Calc: func(_, paramVal string) (T, error) {
+			v, err := strconv.ParseUint(paramVal, 0, mathutil.BitsInType(T(0)))
+			if err != nil {
+				return 0, fmt.Errorf("bad parameter: %q: %w", paramVal, err)
+			}
+			return T(v), nil
+		},
+	}
+}
+
+// Env2StringCalc returns a NamedCalc of the correct type which will return the
+// value of the given environment variable
+func Env2StringCalc[T ~string](name, envName string) NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: name,
+		Calc: func(_, _ string) (T, error) {
+			return T(os.Getenv(envName)), nil
+		},
+	}
+}
+
+// Val2StringCalc returns a NamedCalc of the correct type which will set the
+// value to the value of the parameter value
+func Val2StringCalc[T ~string]() NamedCalc[T] {
+	return NamedCalc[T]{
+		Name: "a parameter value",
+		Calc: func(_, paramVal string) (T, error) {
+			return T(paramVal), nil
+		},
+	}
 }
