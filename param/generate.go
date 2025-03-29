902990e560d96a31d17124b8e2c47a261d76f33f
Nick Wells <wells.nicholas@gmail.com>
2025-03-29 23:14:35 +0000

    build: add the go generate comment for mkfunccontrolparamtype
    this also regenerates the associated file

--- param/generate.go
+++ param/generate.go
@@ -3,3 +3,4 @@
 package param
 
 //go:generate stringer -type=ValueReq
+//go:generate mkfunccontrolparamtype -d "- this determines whether or not a ByName or ByPos param is expected to be set" -for-testing -t ShouldBeSet -v ShouldNotBeSet -v ShouldBeSet
