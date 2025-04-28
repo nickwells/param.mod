a656d4e7fe383d92240a3d5f926d8078aa7e5899
Nick Wells <wells.nicholas@gmail.com>
2025-04-28 11:24:04 +0100

    refactor(param): modernize to use maps and slices funcs
    build the slices of names by using slices.Collect(maps.Keys(...)) rather than
    explicitly in a loop

--- param/suggest.go
+++ param/suggest.go
@@ -1,6 +1,11 @@
 package param
 
-import "github.com/nickwells/strdist.mod/v2/strdist"
+import (
+	"maps"
+	"slices"
+
+	"github.com/nickwells/strdist.mod/v2/strdist"
+)
 
 // SuggestionFunc is the type of a function that returns a slice of suggested
 // alternative names for the given string.
@@ -11,38 +16,26 @@ const alternativeCount = 3
 // SuggestParams finds those parameter names the shortest distance from the
 // passed value and returns them
 func SuggestParams(ps *PSet, s string) []string {
-	names := make([]string, 0, len(ps.nameToParam))
-	for n := range ps.nameToParam {
-		names = append(names, n)
-	}
-
 	finder := strdist.DefaultFinders[strdist.CaseBlindAlgoNameCosine]
 
-	return finder.FindNStrLike(alternativeCount, s, names...)
+	return finder.FindNStrLike(alternativeCount,
+		s, slices.Collect(maps.Keys(ps.nameToParam))...)
 }
 
 // SuggestGroups finds those group names the shortest distance from the
 // passed value and returns them
 func SuggestGroups(ps *PSet, s string) []string {
-	names := make([]string, 0, len(ps.groups))
-	for n := range ps.groups {
-		names = append(names, n)
-	}
-
 	finder := strdist.DefaultFinders[strdist.CaseBlindAlgoNameCosine]
 
-	return finder.FindNStrLike(alternativeCount, s, names...)
+	return finder.FindNStrLike(alternativeCount,
+		s, slices.Collect(maps.Keys(ps.groups))...)
 }
 
 // SuggestNotes finds those note names the shortest distance from the
 // passed value and returns them
 func SuggestNotes(ps *PSet, s string) []string {
-	names := make([]string, 0, len(ps.notes))
-	for n := range ps.notes {
-		names = append(names, n)
-	}
-
 	finder := strdist.DefaultFinders[strdist.CaseBlindAlgoNameCosine]
 
-	return finder.FindNStrLike(alternativeCount, s, names...)
+	return finder.FindNStrLike(alternativeCount,
+		s, slices.Collect(maps.Keys(ps.notes))...)
 }
