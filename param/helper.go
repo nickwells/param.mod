a4cd8ea550d479c45ea5cf02d7560f551fc954a4
Nick Wells <wells.nicholas@gmail.com>
2026-02-06 18:27:39 +0000

    doc(param): expand the documentary comments on the Helper interface
    This should only be of interest to anyone wanting to implement an alternative
    Helper.

--- param/helper.go
+++ param/helper.go
@@ -1,13 +1,24 @@
 package param
 
-// Helper is the interface that a helper object must implement. It should
-// supply a set of default parameters to be added by the AddParams func and a
-// func (called ProcessArgs) to be called after the parsing is complete which
-// will operate on the default parameter values. There should be a Help func
-// for reporting a help message and an error handler for reporting errors.
+// Helper is the interface that a helper object must implement. The Helper is
+// responsible for generating any help messages and for reporting any errors
+// detected while parsing the parameters
 type Helper interface {
-	ProcessArgs(ps *PSet)
-	ErrorHandler(ps *PSet)
-	Help(ps *PSet, messages ...string)
+	// AddParams will add Helper-specific parameters to the PSet. It is
+	// called by the NewSet function after the Helper is set on the
+	// newly created PSet
 	AddParams(ps *PSet)
+	// ProcessArgs is called by PSet.Parse once argument parsing is
+	// complete. It should perform any operations prompted by the parameters
+	// added by the Helper. Note that it is called before any remaining
+	// (non-positional, unnamed) parameters are processed.
+	ProcessArgs(ps *PSet)
+	// ErrorHandler is called by PSet.Parse after the Helper parameters have
+	// been processed and before any remaining parameters are processed. Note
+	// that it is only called if errors have been detected.
+	ErrorHandler(ps *PSet)
+	// Help should generate a help message. It can be called directly through
+	// the PSet.Help method. It will be called by PSet.Parse if the
+	// PSet.HelpRequired method has been called.
+	Help(ps *PSet, messages ...string)
 }
