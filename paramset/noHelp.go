a4240f1ddaa7e4c3cbf8163caeb06528567cf8c1
Nick Wells <wells.nicholas@gmail.com>
2026-02-24 14:47:43 +0000

    feature: added a ptypes.NoHelp type
    This is a minimal implementation of the param.Helper interface.
    
    The paramset.NewNoHelp function uses this type instead of the private version
    of the type it used to have locally defined.

--- paramset/noHelp.go
+++ paramset/noHelp.go
@@ -1,27 +1,9 @@
 package paramset
 
 import (
-	"os"
-
 	"github.com/nickwells/param.mod/v7/param"
+	"github.com/nickwells/param.mod/v7/ptypes"
 )
-
-// noHelp is a minimal implementation of the param.Helper interface. In
-// particular there are no parameters added
-type noHelp struct{}
-
-func (nh noHelp) ProcessArgs(_ *param.PSet)       {}
-func (nh noHelp) Help(_ *param.PSet, _ ...string) {}
-func (nh noHelp) AddParams(_ *param.PSet)         {}
-func (nh noHelp) ErrorHandler(ps *param.PSet) {
-	errMap := ps.Errors()
-	if len(errMap) != 0 {
-		errMap.Report(os.Stderr, ps.ProgName())
-		ps.SetExitStatus(1)
-	}
-}
-
-var nh noHelp
 
 // NewNoHelp creates a new PSet with the helper set to the noHelp
 // helper which does nothing. In particular it will not add any parameters
@@ -36,5 +18,5 @@ var nh noHelp
 // If errors are detected then they will be reported and the program will
 // exit.
 func NewNoHelp(psof ...param.PSetOptFunc) *param.PSet {
-	return param.NewSet(nh, psof...)
+	return param.NewSet(ptypes.NoHelp{}, psof...)
 }
---
+++ ptypes/noHelp.go
@@ -0,0 +1,22 @@
+package ptypes
+
+import (
+	"os"
+
+	"github.com/nickwells/param.mod/v7/param"
+)
+
+// NoHelp is a minimal implementation of the param.Helper interface. In
+// particular there are no parameters added
+type NoHelp struct{}
+
+func (nh NoHelp) ProcessArgs(_ *param.PSet)       {}
+func (nh NoHelp) Help(_ *param.PSet, _ ...string) {}
+func (nh NoHelp) AddParams(_ *param.PSet)         {}
+func (nh NoHelp) ErrorHandler(ps *param.PSet) {
+	errMap := ps.Errors()
+	if len(errMap) != 0 {
+		errMap.Report(os.Stderr, ps.ProgName())
+		ps.SetExitStatus(1)
+	}
+}
