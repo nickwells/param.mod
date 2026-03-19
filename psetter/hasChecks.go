28b521962974b37a78298ce7742b1c9448cbdc0f
Nick Wells <wells.nicholas@gmail.com>
2026-03-19 16:43:18 +0000

    doc(psetter): expand the description of HasChecks
    The extra sentence at the end of the doc-string describes how you might use
    the HasChecks function.

--- psetter/hasChecks.go
+++ psetter/hasChecks.go
@@ -8,7 +8,9 @@ type CheckCounter interface {
 
 // HasChecks returns a string reporting whether or not the number of checks
 // is zero. This is suitable for constructing a string to be returned by an
-// AllowedValues function.
+// AllowedValues function. If you have a Setter which may have checks then
+// add the result of this to the end of the value returned by the
+// AllowedValues method on the Setter.
 func HasChecks(cc CheckCounter) string {
 	if cc.CountChecks() != 0 {
 		return " subject to checks"
